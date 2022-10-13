7084dfd815e71c814b2c5aca2866800cb9faafb8
ledinhvu62 <ledinhvu6@gmail.com>
2022-10-13 08:31:49 +0700

    change API key

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        key: 'AIzaSyDGe80ek36c7vAjDD9rX9utJZhNMpNW5nk'
-        // key: 'AIzaSyAqVe1WifyviIQ72ndJjcKtTj3mwvgfIwk',
+        // key: 'AIzaSyDGe80ek36c7vAjDD9rX9utJZhNMpNW5nk'
+        key: 'AIzaSyAqVe1WifyviIQ72ndJjcKtTj3mwvgfIwk',
         // key: 'AIzaSyCvuJej25d_-vSAtdP8AFtuNIEtTPvT4yI'
         // key: 'AIzaSyAn5KR21i6IADlubXbyk3yYWIA_nGhDZVU'
         // key: 'AIzaSyDOQ72rUVfOTOVWjMyNuZF_CyeT0ZezVio'
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,23 @@
 import firebase from 'firebase/compat/app';
 import "firebase/compat/auth"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDGe80ek36c7vAjDD9rX9utJZhNMpNW5nk",
-  authDomain: "clone-aa236.firebaseapp.com",
-  projectId: "clone-aa236",
-  storageBucket: "clone-aa236.appspot.com",
-  messagingSenderId: "995201836155",
-  appId: "1:995201836155:web:74f8867a18568059fdaede"
-};
-
 // const firebaseConfig = {
-//     apiKey: "AIzaSyAqVe1WifyviIQ72ndJjcKtTj3mwvgfIwk",
-//     authDomain: "ytb-62.firebaseapp.com",
-//     projectId: "ytb-62",
-//     storageBucket: "ytb-62.appspot.com",
-//     messagingSenderId: "234269527645",
-//     appId: "1:234269527645:web:fdb67b71c6b345499547b4"
-// }
+//   apiKey: "AIzaSyDGe80ek36c7vAjDD9rX9utJZhNMpNW5nk",
+//   authDomain: "clone-aa236.firebaseapp.com",
+//   projectId: "clone-aa236",
+//   storageBucket: "clone-aa236.appspot.com",
+//   messagingSenderId: "995201836155",
+//   appId: "1:995201836155:web:74f8867a18568059fdaede"
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAqVe1WifyviIQ72ndJjcKtTj3mwvgfIwk",
+    authDomain: "ytb-62.firebaseapp.com",
+    projectId: "ytb-62",
+    storageBucket: "ytb-62.appspot.com",
+    messagingSenderId: "234269527645",
+    appId: "1:234269527645:web:fdb67b71c6b345499547b4"
+}
 
 // const firebaseConfig = {
 //     apiKey: "AIzaSyCvuJej25d_-vSAtdP8AFtuNIEtTPvT4yI",
